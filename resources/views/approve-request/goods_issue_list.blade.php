d6203463823bc2a09a27f96891c47942be84ac9e
Vendor Integra <vendor.integra@gyssteel.com>
2025-07-16 09:50:57 +0700

    WIP

--- resources/views/approve-request/goods_issue_list.blade.php
+++ resources/views/approve-request/goods_issue_list.blade.php
@@ -1,122 +1,165 @@
-@extends('layouts.app')
-@section('title', 'List '.ucfirst($module))
-@section('content')
 
 <style>
-.content {
-	padding-top:0 !important;
-}
-table.dataTable thead th, table.dataTable thead td {
-    border-bottom: none !important;
-}
-th,td {
-	vertical-align: middle !important;
-}
-.table th {
-	line-height: 2 !important;
-}
-.table-responsive {
-    min-height: .01%;
-    overflow-x: scroll;
-}
-.table {
-	background:#fff;
-	
-}
-.table-striped tbody tr:nth-of-type(odd) {
-    background-color: #ffffff;
-}
-thead {
-	background:#f5f7f9;
-}
-tbody tr:first-child td {
-	border-top:1px solid #cfd7df !important;
-}
-tr th {
-	border-top:1px solid #cfd7df !important;
-}
-tbody tr td {
-	border-right:none !important
-}
-tr {
-	border-right:1px solid #cfd7df !important;
-	border-left:1px solid #cfd7df !important;
-}
-tr td:first-child,tr th:first-child {
-	border-left:1px solid #cfd7df !important;
-}
-tr th {
-	border-right:1px solid #cfd7df !important;
-	white-space:nowrap;
-}
+    .content {
+        padding-top: 0 !important;
+    }
+
+    table.dataTable thead th,
+    table.dataTable thead td {
+        border-bottom: none !important;
+    }
+
+    th,
+    td {
+        vertical-align: middle !important;
+    }
+
+    .table th {
+        line-height: 2 !important;
+    }
+
+    .table-responsive {
+        min-height: .01%;
+        overflow-x: scroll;
+    }
+
+    .table {
+        background: #fff;
+
+    }
+
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: #ffffff;
+    }
+
+    thead {
+        background: #f5f7f9;
+    }
+
+    tbody tr:first-child td {
+        border-top: 1px solid #cfd7df !important;
+    }
+
+    tr th {
+        border-top: 1px solid #cfd7df !important;
+    }
+
+    tbody tr td {
+        border-right: none !important
+    }
+
+    tr {
+        border-right: 1px solid #cfd7df !important;
+        border-left: 1px solid #cfd7df !important;
+    }
+
+    tr td:first-child,
+    tr th:first-child {
+        border-left: 1px solid #cfd7df !important;
+    }
+
+    tr th {
+        border-right: 1px solid #cfd7df !important;
+        white-space: nowrap;
+    }
 
 </style>
 
 
-<div class="container-fluid">
-    @include('ticket_navigation')
-
-    <div class="table-responsive" style="display: block">
-        <?php
+<div class="table-responsive pl-3 pr-3" style="display: block">
+    <?php
             $identifier =  rand(pow(10, 12-1), pow(10, 12)-1);
             $id_table = "table-".$identifier; 
         ?>
-        <style>
+    <style>
         th {
-                font-weight:600 !important;
+            font-weight: 600 !important;
         }
-        </style>            
-        @include('flash::message')
-        
-        <table class="table table-striped table-bordered" id="main-table">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Request No</th>
-                    <th>Requestor Name</th>
-                    <th>Subject</th>
-                    <th>Type</th>
-                    <th>Status</th>
-                    <th>Created Date</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
+
+    </style>
+    @include('flash::message')
+
+    <table class="table table-striped table-bordered" id="goods-issue-table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Request No</th>
+                <th>Requestor Name</th>
+                <th>Subject</th>
+                <th>Type</th>
+                <th>Status</th>
+                <th>Created Date</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+    </table>
 </div>
-@endsection
 
 @section('js')
 <script>
-$(document).ready( function () {
-    $('#main-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route($module.'.index') }}",
-        columns: [
-            { data: 'id', name: 'id' },
-            { data: 'code', name: 'code' },
-            { data: 'created_by_user.name', name: 'created_by_user.name', searchable: false, orderable: false },
-            { data: 'subject', name: 'subject' },
-            { data: 'inventory_type.title', name: 'inventory_type.title' },
-            { data: 'status_label', name: 'status_label' },
-            { data: 'created_at', name: 'created_at', render: (data, type, row) => row.created_date},
-            { data: 'action', name: 'action', orderable: false, sortable: false },
-        ]
-	});
- });
+    $(document).ready(function () {
+        $('#goods-issue-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('goods_issue.index') }}",
+            columns: [{
+                    data: 'id',
+                    name: 'id'
+                },
+                {
+                    data: 'code',
+                    name: 'code'
+                },
+                {
+                    data: 'created_by_user.name',
+                    name: 'created_by_user.name',
+                    searchable: false,
+                    orderable: false
+                },
+                {
+                    data: 'subject',
+                    name: 'subject'
+                },
+                {
+                    data: 'inventory_type.title',
+                    name: 'inventory_type.title'
+                },
+                {
+                    data: 'status_label',
+                    name: 'status_label'
+                },
+                {
+                    data: 'created_at',
+                    name: 'created_at',
+                    render: (data, type, row) => row.created_date
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: false,
+                    sortable: false
+                },
+            ]
+        });
+    });
+
 </script>
 @endsection
 <style>
-tfoot input {
-	width:100%;
-	border-radius: 2px;
-	border: 1px solid #e2e7f1;
-	box-shadow: none;
+    #goods-issue-table {
+        width: 100% !important
+    }
+    tfoot input {
+        width: 100%;
+        border-radius: 2px;
+        border: 1px solid #e2e7f1;
+        box-shadow: none;
 
-}
-tfoot input:focus {
-  
-  border: 1px solid #e2e7f1;
-}
+    }
+
+    tfoot input:focus {
+
+        border: 1px solid #e2e7f1;
+    }
+
 </style>
